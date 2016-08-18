5fc3bb94f2f3b978d289ea1885c63359dbc4c39e
chenggoi <942824866@qq.com>
2016-08-18 18:29:35 +0800

    [20160818] ChooseAreaActivity

--- app/src/main/java/com/chenggoi/androidstudy/CoolWeather/activity/ChooseAreaActivity.java
+++ app/src/main/java/com/chenggoi/androidstudy/CoolWeather/activity/ChooseAreaActivity.java
@@ -3,16 +3,21 @@ package com.chenggoi.androidstudy.CoolWeather.activity;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chenggoi.androidstudy.CoolWeather.model.City;
 import com.chenggoi.androidstudy.CoolWeather.model.CoolWeatherDB;
 import com.chenggoi.androidstudy.CoolWeather.model.County;
 import com.chenggoi.androidstudy.CoolWeather.model.Province;
+import com.chenggoi.androidstudy.CoolWeather.util.HttpCallbackListener;
+import com.chenggoi.androidstudy.CoolWeather.util.HttpUtil;
+import com.chenggoi.androidstudy.CoolWeather.util.Utility;
 import com.chenggoi.androidstudy.R;
 
 import java.util.ArrayList;
@@ -26,6 +31,8 @@ public class ChooseAreaActivity extends Activity {
     public static final int LEVEL_PROVINCE = 0;
     public static final int LEVEL_CITY = 1;
     public static final int LEVEL_COUNTY = 2;
+
+    public static final String WEATHER_ADDRESS = "http://www.weather.com.cn/data/city3jdata/";
     private TextView titleText;
     private ListView listView;
 
@@ -68,14 +75,125 @@ public class ChooseAreaActivity extends Activity {
     }
 
     private void queryProvinces() {
-        
+        provinceList = coolWeatherDB.loadProvinces();
+        if (provinceList.size() > 0) {
+            dataList.clear();
+            for (Province p : provinceList) {
+                dataList.add(p.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            titleText.setText("China");
+            currentLevel = LEVEL_PROVINCE;
+        } else {
+            queryFromServer(null, "province");
+        }
     }
 
     private void queryCities() {
-
+        cityList = coolWeatherDB.loadCities(selectedProvince.getId());
+        if (cityList.size() > 0) {
+            dataList.clear();
+            for (City c : cityList) {
+                dataList.add(c.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            titleText.setText(selectedProvince.getProvinceName());
+            currentLevel = LEVEL_CITY;
+        } else {
+            queryFromServer(selectedProvince.getProvinceCode(), "city");
+        }
     }
 
     private void queryCounties() {
+        countyList = coolWeatherDB.loadCounties(selectedCity.getId());
+        if (countyList.size() > 0) {
+            dataList.clear();
+            for (County c : countyList) {
+                dataList.add(c.getCountyName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            titleText.setText(selectedCity.getCityName());
+            currentLevel = LEVEL_COUNTY;
+        } else {
+            queryFromServer(selectedCity.getCityCode(), "county");
+        }
+    }
 
+    private void queryFromServer(final String code, final String type) {
+        String address;
+        if (!TextUtils.isEmpty(code)) {
+            address = WEATHER_ADDRESS + "provshi/" + code + ".html";
+        } else {
+            address = WEATHER_ADDRESS + "china.html";
+        }
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvincesResponse(coolWeatherDB, response);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCitiesResponse(coolWeatherDB, response, selectedProvince.getId());
+                } else if ("county".equals(type)) {
+                    result = Utility.handleCountiesResponse(coolWeatherDB, response, selectedCity.getId());
+                }
+                if (result) {
+                    //return to UI thread
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if ("province".equals(type)) {
+                                queryProvinces();
+                            } else if ("city".equals(type)) {
+                                queryCities();
+                            } else if ("county".equals(type)) {
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(ChooseAreaActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void showProgressDialog() {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Loading...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
+    }
+
+    private void closeProgressDialog() {
+        if (progressDialog != null) {
+            progressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (currentLevel == LEVEL_COUNTY) {
+            queryCities();
+        } else if (currentLevel == LEVEL_CITY) {
+            queryProvinces();
+        } else if (currentLevel == LEVEL_PROVINCE) {
+            finish();
+        }
     }
 }
